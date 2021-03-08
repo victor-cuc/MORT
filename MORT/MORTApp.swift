86080be0a249e2495c42831811ac5ef5191edefd
Victor Cuc <victorcuc99@yahoo.com>
2021-03-08 16:54:42 +0200

    Initial Commit

---
+++ MORT/MORTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MORTApp.swift
+//  MORT
+//
+//  Created by Victor Cuc on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MORTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
