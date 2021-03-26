51c6392cc8e3b9a5d3a5bfe2e0303805bebb44d2
Victor Cuc <victorcuc99@yahoo.com>
2021-03-26 12:19:13 +0200

    ContentView: basic list view with horizontal scrolling

--- MORT/ContentView.swift
+++ MORT/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+  
+  init() {
+    UITableViewHeaderFooterView.appearance().tintColor = UIColor.clear
+  }
+  var body: some View {
+    NavigationView {
+      List {
+        HStack {
+          Text("Popular")
+            .font(.title2)
+            .bold()
             .padding()
+          Spacer()
+        }
+        ScrollView (.horizontal, showsIndicators: false) {
+          HStack {
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+            Text("route")
+          }
+        }.frame(height: 100)
+        HStack {
+          Text("Community")
+            .font(.title2)
+            .bold()
+            .padding()
+          Spacer()
+        }
+        ForEach(1..<20) { _ in Text("Pulle")}
+      }
+      .navigationBarTitle("Routes")
+      .toolbar {
+        ToolbarItem(placement: .navigationBarLeading) {
+          Button(action:{
+            
+          }, label: {
+            Image(systemName: "plus")
+          })
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+      .previewedInAllColorSchemes
+  }
+}
+
+extension View {
+  var previewedInAllColorSchemes: some View {
+    ForEach(
+      ColorScheme.allCases,
+      id: \.self,
+      content: preferredColorScheme
+    )
+  }
 }
